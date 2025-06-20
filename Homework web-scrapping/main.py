a43dcc8e1e7a7856edad26774120dadb382e3139
St <valuable_information@mail.ru>
2025-06-20 14:22:16 +0300

    add hw w-s-ing again

--- Homework web-scrapping/main.py
+++ Homework web-scrapping/main.py
@@ -1,13 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 
-# Определяем список ключевых слов:
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
+KEYWORDS = ['криптовалюты', 'жизнь', 'OpenAI', 'ресурс', 'лабубу'] # Определяем список ключевых слов:
 
 url = 'https://habr.com/ru/articles/' # url для парсинга
-
 
 response = requests.get(url)
 response.raise_for_status()  # Проверяем, успешен ли запрос
@@ -24,8 +21,29 @@ for article in articles:
     title = title_tg.get_text(strip=True) # наш заголовочек статьи
     link = 'https://habr.com' + title_tg['href']
 
-    print(f"Title: {title}")
-    print(f"link: {link}")
-    print('---------')
+    date_tg = article.find('time')
+    date = date_tg['datetime'][:10] if date_tg else 'Дата не найдена' # дата нашей статьи (вез времени) формат year-month-day
 
-print('мда треш')
+    article_response = requests.get(link)
+    article_response.raise_for_status()
+    article_soup = BeautifulSoup(article_response.text, 'html.parser')
+
+    body = article_soup.find('div', {'class': lambda c: c and 'article-formatted-body' in c}) # Ищем основной текст статьи
+    full_text = body.get_text(strip=True).lower() if body else ''
+
+    cot = article_soup.find('div', class_='tm-publication-hubs')
+    tags = []
+    if cot:
+        tag_links = cot.find_all('a', class_='tm-publication-hub__link')
+        tags = [tag.get_text(strip=True).lower() for tag in tag_links]
+
+    # Проверяем наличие любого из ключевых слов в заголовке, тексте или тегах
+    combined_text = (title + ' ' + full_text + ' ' + ' '.join(tags)).lower()
+    if any(keyword in combined_text for keyword in KEYWORDS):
+        print(f"{date} – {title} – {link}")
+    
+
+    # print(f"Title: {title}")
+    # print(f"link: {link}")
+    # print(f"date: {date}")
+    # print('---------')
